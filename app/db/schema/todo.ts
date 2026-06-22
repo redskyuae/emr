3ff0f774a4a3807437c45b7e884ef4c8e723bf18
Rakesh Mirtha <mirta.srm@gmail.com>
2026-06-22 14:27:29 +0530

    refactor: remove todo module and drop associated database table

--- app/db/schema/todo.ts
+++
@@ -1,8 +0,0 @@
-import { boolean, integer, pgTable, text, varchar } from 'drizzle-orm/pg-core';
-
-export const todoTable = pgTable('todo', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  title: varchar({ length: 255 }).notNull(),
-  description: text().notNull().default(''),
-  isCompleted: boolean('is_completed').notNull().default(false),
-});
